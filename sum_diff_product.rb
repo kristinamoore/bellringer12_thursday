e28a3d4cdba58a73b648ab2f0174a1ef0dcdd449
kristina moore <kmoore@chicagotechacademy.org>
2013-11-09 14:54:20 -0600

    classwork friday

--- sum_diff_product.rb
+++ sum_diff_product.rb
@@ -1,11 +1,14 @@
-def method
+def method 
 
-
-puts"Give me two numbers"
+puts"Give me one number"
 
 nom121 = gets.chomp.to_i
+ puts"Give me the second number"
 nom50 = gets.chomp.to_i
 
 puts nom121+nom50
 puts nom121-nom50
 puts nom121*nom50
+end
+
+method
